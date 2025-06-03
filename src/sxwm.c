7f79463c7b1881bdf1e7a76ebfd8d2a2e46213f4
werdl <werdl_@outlook.com>
2025-06-03 08:10:17 +0100

    Remove autostart-rerunning from reload_config

--- src/sxwm.c
+++ src/sxwm.c
@@ -1144,33 +1144,6 @@ void reload_config(void)
 		fprintf(stderr, "sxrc: error parsing config file\n");
 		init_defaults();
 	}
-    
-    for (int i = 0; i < 256; i++) {
-        if (user_config.torun[i]) {
-            printf("[DEBUG] executing %s\n", user_config.torun[i]);
-            pid_t pid = fork();
-			if (pid == 0) {
-				char *argv[256];
-				int j = 0;
-				char *arg = strtok(user_config.torun[i], " ");
-				while (arg && j < 256) {
-					argv[j++] = arg;
-					arg = strtok(NULL, " ");
-				}
-				argv[j] = NULL;
-				execvp(argv[0], argv);
-				perror("execvp");
-				_exit(127);
-			}
-			else if (pid > 0) {
-				// parent: don’t wait, just continue (background)
-			}
-			else {
-				perror("fork");
-			} 
-        }
-    }
-
 	grab_keys();
 	XUngrabButton(dpy, AnyButton, AnyModifier, root);
 	XGrabButton(dpy, Button1, user_config.modkey, root, True, ButtonPressMask | ButtonReleaseMask | PointerMotionMask,
@@ -1302,8 +1275,8 @@ void setup(void)
 			else {
 				perror("fork");
 			} 
-        }
-    }
+        	}
+    	}
 	grab_keys();
 
 	c_normal = XcursorLibraryLoadCursor(dpy, "left_ptr");
